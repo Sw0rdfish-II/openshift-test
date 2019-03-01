693c10084a280dc7b567f1ccd85e9bf35b69f003
azhante <andreas.tellenbach@aspectra.ch>
2019-03-01 11:18:22 +0100

    modified:   index.php

--- test-site/index.php
+++ test-site/index.php
@@ -6,3 +6,18 @@
 		<h1><?php echo 'You\'re on the: =>' .$_ENV["STAGE"] . '<= Environment!'; ?></h1>
         </body>
 </html>
+
+<?php
+$conn = pg_connect("host=$DB_HOST port=$DB_PORT dbname=$DB_NAME user=$DB_USER password=$DB_PASSWORD");
+if (!$conn) {
+  echo "PostgreSQL-DB-Verbindung fehlgeschlangen.\n";
+  exit;
+}
+
+$result = pg_query($conn, "SELECT * FROM customers");
+if (!$result) {
+  echo "Query fehlgeschlagen.\n";
+  exit;
+}
+
+?>
