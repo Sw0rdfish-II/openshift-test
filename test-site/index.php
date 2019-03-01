6b8b3af54622e9acc262509a6d268bff4a5cf5c7
azhante <andreas.tellenbach@aspectra.ch>
2019-03-01 10:14:56 +0100

    modified:   index.php

--- test-site/index.php
+++ test-site/index.php
@@ -2,7 +2,6 @@
         <head><title>OpenShift Webhooks work!</title></head>
         <body>
                 <h1>Wohoooo it works!</h1><br>
-                <h1>New Feature!!!!!! Hernandez rules af!</h1><br>
                 <img src="animation.gif"><br>
 		<h1><?php echo 'You\'re on the: =>' .$_ENV["STAGE"] . '<= Environment!'; ?></h1>
         </body>
