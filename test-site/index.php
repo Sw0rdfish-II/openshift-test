78dbee039e16dfd3b73d2c7ed173f7dc7096953c
Sw0rdfish-II <47419457+Sw0rdfish-II@users.noreply.github.com>
2020-01-15 10:29:25 +0100

    Update index.php

--- test-site/index.php
+++ test-site/index.php
@@ -2,6 +2,7 @@
         <head><title>OpenShift Webhooks work!</title></head>
         <body>
                 <h1>Wohoooo it works! LOOOOLz</h1><br>
+		<h1>Adrian is the Boss</h1>
                 <img src="animation.gif"><br>
 		<h1><?php echo 'You\'re on the: =>' .$_ENV["STAGE"] . '<= Environment!'; ?></h1>
         </body>
